6fca0e18e0bb09144c3ef3e32cdb02112549e1e0
Bob Hyman <bob.hyman@gmail.com>
2025-12-25 09:57:11 -0500

    Don't enable sourcemaps in production builds

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,17 +3,22 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
-export default defineConfig({
-  plugins: [tailwindcss(), svelte()],
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    tailwindcss(),
+    svelte({
+
+    }),
+  ],
   resolve: {
     alias: {
       $lib: resolve("./src/lib"),
     },
   },
   build: {
-    sourcemap: true,
+    sourcemap: mode === "development" ? "inline" : false,
   },
   css: {
-    devSourcemap: true,
+    devSourcemap: mode === "development",
   },
-});
+}));

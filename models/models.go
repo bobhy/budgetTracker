be4d4b9ead8e30281a628dfae3c1aac6647a1654
Bob Hyman <bob.hyman@gmail.com>
2026-02-05 11:13:41 -0500

    Get foreign key relations right (maybe?)

--- models/models.go
+++ models/models.go
@@ -17,23 +17,13 @@ type SortOption struct {
 
 type Beneficiary struct {
 	Name string `gorm:"primaryKey;default:'None';constraint:OnUpdate:CASCADE,OnDelete:SET DEFAULT"`
-
-	// Relationships
-	Accounts        []Account        `gorm:"foreignKey:Beneficiary;references:Name"`
-	Budgets         []Budget         `gorm:"foreignKey:Beneficiary;references:Name"`
-	Transactions    []Transaction    `gorm:"foreignKey:Beneficiary;references:Name"`
-	RawTransactions []RawTransaction `gorm:"foreignKey:Beneficiary;references:Name"`
 }
 
 type Account struct {
-	Name        string `gorm:"primaryKey;default:'None';constraint:OnUpdate:CASCADE,OnDelete:SET DEFAULT"`
-	Description string
-	Beneficiary string
-
-	// Relationships
-	Budgets         []Budget         `gorm:"foreignKey:Beneficiary;references:Name"`
-	Transactions    []Transaction    `gorm:"foreignKey:Account;references:Name"`
-	RawTransactions []RawTransaction `gorm:"foreignKey:Account;references:Name"`
+	Name           string `gorm:"primaryKey;default:'None';constraint:OnUpdate:CASCADE,OnDelete:SET DEFAULT"`
+	Description    string
+	Beneficiary    string
+	BeneficiaryObj *Beneficiary `gorm:"foreignKey:Beneficiary;references:Name"`
 }
 
 // Budget represents a planned expenditure over time
@@ -62,42 +52,45 @@ type Budget struct {
 	Name           string `gorm:"primaryKey;default:'None';constraint:OnUpdate:CASCADE,OnDelete:SET DEFAULT"`
 	Description    string
 	Beneficiary    string
+	BeneficiaryObj *Beneficiary `gorm:"foreignKey:Beneficiary;references:Name"`
 	Amount         Money
 	IntervalMonths int
-
-	// Relationships
-	Transactions    []Transaction    `gorm:"foreignKey:Budget;references:Name"`
-	RawTransactions []RawTransaction `gorm:"foreignKey:Budget;references:Name"`
 }
 
 type Transaction struct {
-	ID          uint       `gorm:"primarykey"`
-	CreatedAt   time.Time  `json:"-"` // Hide from frontend to avoid warning/binding issues
-	UpdatedAt   time.Time  `json:"-"`
-	DeletedAt   *time.Time `gorm:"index"` // Use pointer to time for soft delete, hide from json
-	PostedDate  Date
-	Account     string `gorm:"foreignKey:Account"`
-	Amount      Money
-	Description string
-	Tag         string
-	Budget      string
-	Beneficiary string // Overrides Account default if set
-	RawHint     string // Category hint from import
+	ID             uint       `gorm:"primarykey;autoIncrement"`
+	CreatedAt      time.Time  `json:"-"` // Hide from frontend to avoid warning/binding issues
+	UpdatedAt      time.Time  `json:"-"`
+	DeletedAt      *time.Time `gorm:"index"` // Use pointer to time for soft delete, hide from json
+	PostedDate     Date
+	Account        string
+	AccountObj     *Account `gorm:"foreignKey:Account;references:Name"`
+	Amount         Money
+	Description    string
+	Tag            string
+	Budget         string
+	BudgetObj      *Budget `gorm:"foreignKey:Budget;references:Name"`
+	Beneficiary    string
+	BeneficiaryObj *Beneficiary `gorm:"foreignKey:Beneficiary;references:Name"` // Overrides Account default if set
+	RawHint        string       // Category hint from import
 }
 
 // RawTransaction is used for importing transactions before they are fully processed and linked
 type RawTransaction struct {
-	ID          uint       `gorm:"primarykey"`
-	CreatedAt   time.Time  `json:"-"`
-	UpdatedAt   time.Time  `json:"-"`
-	DeletedAt   *time.Time `gorm:"index"`
-	PostedDate  Date
-	Account     string `gorm:"foreignKey:Account"`
-	Amount      Money
-	Description string
-	Tag         string
-	Budget      string `gorm:"foreignKey:Budget"`
-	Action      string // "add" or "update"
-	Beneficiary string `gorm:"foreignKey:Beneficiary"`
-	RawHint     string
+	ID             uint       `gorm:"primarykey;autoIncrement"`
+	CreatedAt      time.Time  `json:"-"`
+	UpdatedAt      time.Time  `json:"-"`
+	DeletedAt      *time.Time `gorm:"index"`
+	PostedDate     Date
+	Account        string
+	AccountObj     *Account `gorm:"foreignKey:Account;references:Name"`
+	Amount         Money
+	Description    string
+	Tag            string
+	Budget         string
+	BudgetObj      *Budget `gorm:"foreignKey:Budget;references:Name"`
+	Action         string  // "add" or "update"
+	Beneficiary    string
+	BeneficiaryObj *Beneficiary `gorm:"foreignKey:Beneficiary;references:Name"`
+	RawHint        string
 }
